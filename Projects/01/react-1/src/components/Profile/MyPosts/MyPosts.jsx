d7f104f87db049d8c48a153548789b864d7ad4ca
Serhii Spytsia <sergey.sp.1991@gmail.com>
2021-11-30 23:01:09 +0200

    inserted Post in MyPosts

--- Projects/01/react-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ Projects/01/react-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,24 @@
 import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
+  let postsElements = props.posts.map((p) => (
+    <Post message={p.message} like={p.like} />
+  ));
+
   return (
-    <form>
-      <label className={s.posts} for="content">
-        My posts
-      </label>
-      <textarea></textarea>
-      <button className={s.btn} type="submit">
-        Add post
-      </button>
-    </form>
+    <>
+      <form>
+        <label className={s.post} for="content">
+          My posts
+        </label>
+        <textarea></textarea>
+        <button className={s.btn} type="submit">
+          Add post
+        </button>
+      </form>
+      <div className={s.posts}>{postsElements}</div>
+    </>
   );
 };
 
--- Projects/01/react-1/src/components/Profile/Profile.jsx
+++ Projects/01/react-1/src/components/Profile/Profile.jsx
@@ -1,18 +1,12 @@
 import MyPosts from "./MyPosts/MyPosts";
-import Post from "./MyPosts/Post/Post";
 import s from "./Profile.module.css";
 import ProfileUser from "./ProfileUser/ProfileUser";
 
 const Profile = (props) => {
-  let postsElements = props.posts.map((p) => (
-    <Post message={p.message} like={p.like} />
-  ));
-
   return (
     <div>
       <ProfileUser />
-      <MyPosts />
-      <div className={s.posts}>{postsElements}</div>
+      <MyPosts posts={props.posts} />
     </div>
   );
 };
