a1d62b3719718e7fbfeff5c59da99cfabac1f49c
Serhii Spytsia <sergey.sp.1991@gmail.com>
2021-12-01 12:40:30 +0200

    avatar friends

--- Projects/01/react-1/src/components/Dialogs/DialogItem/DialogItem.jsx
+++ Projects/01/react-1/src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -6,7 +6,13 @@ const DialogItem = (props) => {
 
   return (
     <div className={s.item}>
-      <NavLink to={path}>{props.name}</NavLink>
+      <NavLink to={path}>
+        <img
+          className={s.avatar}
+          src="http://cs622426.vk.me/v622426834/409d/baLqspYwi84.jpg"
+        ></img>
+        {props.name}
+      </NavLink>
     </div>
   );
 };
