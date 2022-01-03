2985462c45945995c162d3bd00f03b4d349c0513
Serhii Spytsia <sergey.sp.1991@gmail.com>
2022-01-03 16:13:37 +0200

    Login

--- Projects/01/react-1/src/components/Login/Login.jsx
+++ Projects/01/react-1/src/components/Login/Login.jsx
@@ -1,7 +1,74 @@
+import { Formik } from "formik";
+import * as yup from "yup";
+import s from "./Login.module.css";
+
+const LoginForm = (props) => {
+  const validationSchema = yup.object().shape({
+    login: yup
+      .string()
+      .typeError("Должно быть строкой")
+      .required("Обязательно"),
+  });
+
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          login: "",
+          password: "",
+          rememberMe: "",
+        }}
+        validateOnBlur
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        validationSchema={validationSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          isValid,
+          handleSubmit,
+          dirty,
+        }) => (
+          <div>
+            <label className={s.post} htmlFor="login">
+              Login
+            </label>
+            <input
+              className={s.input}
+              type={"text"}
+              name={"login"}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.login}
+            ></input>
+            {touched.login && errors.login && (
+              <p className={s.error}>{errors.login}</p>
+            )}
+            <button
+              disabled={!isValid && !dirty}
+              onClick={handleSubmit}
+              className={s.btn}
+              type={"submit"}
+            >
+              LogIn
+            </button>
+          </div>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
 const Login = () => {
   return (
     <div>
       <h1>LOGIN</h1>
+      <LoginForm />
     </div>
   );
 };
