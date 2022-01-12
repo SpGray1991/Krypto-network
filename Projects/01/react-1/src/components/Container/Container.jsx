35d88b5ba91747dc7ddc2043d5e28ba9efc7f6e3
Serhii Spytsia <sergey.sp.1991@gmail.com>
2022-01-12 13:23:36 +0200

    suspense lazy

--- Projects/01/react-1/src/components/Container/Container.jsx
+++ Projects/01/react-1/src/components/Container/Container.jsx
@@ -1,22 +1,33 @@
+import React, { Suspense } from "react";
 import s from "../Container/Container.module.css";
 import { Routes, Route } from "react-router-dom";
-import DialogsContainer from "../Dialogs/DialogsContainer";
+/* import DialogsContainer from "../Dialogs/DialogsContainer"; */
 import NavbarContainer from "../Navbar/NavbarContainer";
 import UsersContainer from "../Users/UsersContainer";
-import ProfileContainer from "../Profile/ProfileContainer";
+/* import ProfileContainer from "../Profile/ProfileContainer"; */
 import LoginPage from "../Login/Login";
+import Preloader from "../Common/Preloader/Preloader";
+
+const DialogsContainer = React.lazy(() =>
+  import("../Dialogs/DialogsContainer")
+);
+const ProfileContainer = React.lazy(() =>
+  import("../Profile/ProfileContainer")
+);
 
 const Container = () => {
   return (
     <container className={s.container}>
       <NavbarContainer />
       <content className={s.content}>
-        <Routes>
-          <Route path="/profile/*" element={<ProfileContainer />} />
-          <Route path="/dialogs/*" element={<DialogsContainer />} />
-          <Route path="/users/*" element={<UsersContainer />} />
-          <Route path="/login/*" element={<LoginPage />} />
-        </Routes>
+        <Suspense fallback={<Preloader />}>
+          <Routes>
+            <Route path="/profile/*" element={<ProfileContainer />} />
+            <Route path="/dialogs/*" element={<DialogsContainer />} />
+            <Route path="/users/*" element={<UsersContainer />} />
+            <Route path="/login/*" element={<LoginPage />} />
+          </Routes>
+        </Suspense>
       </content>
     </container>
   );
